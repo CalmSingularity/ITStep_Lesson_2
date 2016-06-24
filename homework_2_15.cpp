d1bd93a2ddbbdb4e6b0622a802c6da8443e2763c
Semen Klimov <simon-klimov@yandex.ru>
2016-06-25 00:56:34 +0300

    Improved task 15 programm logic by eliminating excessive loops

--- homework_2_15.cpp
+++ homework_2_15.cpp
@@ -4,27 +4,17 @@ using namespace std;
 // Multiplication table
 int main (int argc, char** argv) {
   
-  // First part: A equals 1 to 5
-  for (int b = 1; b <= 9; b++) {
-    for (int a = 1; a <= 5; a++) {
-      cout << a << " * " << b << " = " << (a * b) << "   ";
+  for (int a_shift = 0; a_shift < 10; a_shift = a_shift + 5) {
+    for (int b = 1; b <= 9; b++) {
+      for (int a = a_shift + 1; a <= a_shift + 5; a++) {
+        cout << a << " * " << b << " = " << (a * b) << "   ";
 
-      // improve output formatting with additional spacing:
-      if (a * b < 10) cout << " ";
-    }
-    cout << endl;
+        // improve output formatting with additional spacing:
+        if (a * b < 10) cout << " ";
+      }
+      cout << endl;
   }
   cout << endl;
-
-  // Second part: A equals 6 to 10
-  for (int b = 1; b <= 9; b++) {
-    for (int a = 6; a <= 10; a++) {
-      cout << a << " * " << b << " = " << (a * b) << "   ";
-
-      // improve output formatting with additional spacing:
-      if (a * b < 10) cout << " ";
-    }
-    cout << endl;
   }
   
   return 0;
